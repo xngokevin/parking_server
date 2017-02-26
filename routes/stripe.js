1f2833441ecdd6994c443b26394dbaccd86d8316
Kevin Ngo <xngokevin@yahoo.com>
2017-02-26 15:57:33 -0800

    Logging for stripe fil;e

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,4 +1,4 @@
-var express  = require('express');
+var express = require('express');
 var app = express();
 var router = express.Router();
 
@@ -19,6 +19,17 @@ var jwt = require('jsonwebtoken');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
 
+// Logger
+var winston = require('winston');
+var logger = new(winston.Logger)({
+  transports: [
+    new(winston.transports.Console)(),
+    new(winston.transports.File)({
+      filename: 'logs/stripe.log'
+    })
+  ]
+});
+
 //Parse application/x-www-form-urlencoded
 router.use(bodyParser.urlencoded({
   extended: true
@@ -35,22 +46,22 @@ router.use(function(req, res, next) {
 
   // decode token
   if (token) {
-    	// verifies secret and checks exp
-    	jwt.verify(token, config.token_secret, function(err, decoded) {
-      	if (err) {
-        	return next(error_msg.global.invalid_token);
-      	}
-      	else {
-        	// if everything is good, save to request for use in other routes
-        	req.decoded = decoded;
-        		next();
-      		}
-    	});
+    // verifies secret and checks exp
+    jwt.verify(token, config.token_secret, function(err, decoded) {
+      if (err) {
+        return next(error_msg.global.invalid_token);
+      }
+      else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
 
-  	}
-  	else {
-	    return next(error_msg.global.no_token);
-  	}
+  }
+  else {
+    return next(error_msg.global.no_token);
+  }
 });
 
 //Apply to routes that require authorization
@@ -58,16 +69,17 @@ app.use('/', router);
 
 //Create customer
 router.post('/customer', function(req, res, next) {
-  if(!req.decoded.email) {
+  if (!req.decoded.email) {
     return next(error_msg.stripe.customer_no_email);
   }
 
   stripe.customers.create({
-    description:'',
+    description: '',
     email: req.body.email
   }, function(err, customer) {
-    if(err) {
-      return next(error_mst.stripe.customer_create);
+    if (err) {
+    	logger.log('error', err);
+      return next(error_msg.stripe.customer_create);
     }
     else {
       res.send(success_msg.stripe.customer_create);
@@ -77,19 +89,20 @@ router.post('/customer', function(req, res, next) {
 
 //Retrieve card
 router.get('/card', function(req, res, next) {
-	stripe.customers.listCards(
-		req.decoded.customer_id, {
-			limit: 1
-		},
-		function(err, cards) {
-			if(err) {
-				return next(error_msg.stripe.card_retrieve);
-			}
-			else {
-				res.send(cards.data);
-			}
-		}
-	)
+  stripe.customers.listCards(
+    req.decoded.customer_id, {
+      limit: 1
+    },
+    function(err, cards) {
+      if (err) {
+      	logger.log('error', err);
+        return next(error_msg.stripe.card_retrieve);
+      }
+      else {
+        res.send(cards.data);
+      }
+    }
+  )
 })
 
 //Create card
@@ -98,19 +111,21 @@ router.post('/card', function(req, res, next) {
   card.object = "card";
 
   stripe.customers.createSource(
-    req.decoded.customer_id,
-    {source: card},
+    req.decoded.customer_id, {
+      source: card
+    },
     function(err, card) {
-      if(err) {
-        if(err.type == "StripeCardError") {
-        	return res.send({
-        		status: 402,
-        		type: err.type,
-        		message: err.message
-        	})
+      if (err) {
+        if (err.type == "StripeCardError") {
+          return res.send({
+            status: 402,
+            type: err.type,
+            message: err.message
+          })
         }
         else {
-			return next(error_msg.stripe.card_create);
+        	logger.log('error', err);
+          return next(error_msg.stripe.card_create);
         }
       }
       else {
@@ -121,70 +136,66 @@ router.post('/card', function(req, res, next) {
 });
 
 router.delete('/card', function(req, res, next) {
-	stripe.customers.deleteCard(
-		req.decoded.customer_id,
-		req.query.id,
-		function(err, confirmation) {
-			if(err) {
-				return next(error_msg.stripe.card_delete);
-			}
-			else {
-				res.send(success_msg.stripe.card_delete);
-			}
-		}
-	);
+  stripe.customers.deleteCard(
+    req.decoded.customer_id,
+    req.query.id,
+    function(err, confirmation) {
+      if (err) {
+      	logger.log('error', err);
+        return next(error_msg.stripe.card_delete);
+      }
+      else {
+        res.send(success_msg.stripe.card_delete);
+      }
+    }
+  );
 });
 
 //Create Transaction
 router.post('/charge', function(req, res, next) {
-  /*
-  Request Body for now.
-    req.body: {
-      parking_id: 1,
-      hours: 1
-    }
-  */
-  console.log(req.body);
 
-  if(!req.body.parking_id) {
+	logger.log('info', req.decoded.email + ": Create charge");
+
+  if (!req.body.parking_id) {
     return next(error_msg.stripe.no_parking_id);
   }
 
-  if(!req.body.hours) {
+  if (!req.body.hours) {
     return next(error_msg.stripe.no_parking_hours);
   }
 
-  if(!req.body.location_id){
+  if (!req.body.location_id) {
     return next(error_msg.stripe.no_location_id);
   }
 
+	logger.log('info', req.decoded.email + ": location_id = " + req.body.parking_id + " hours = " + req.body.hours);
+
   var select_query = "SELECT * FROM parking_space WHERE location_id = ? AND space_id = ?";
   parking_db.getConnection(function(err, connection) {
-    if(err) {
-      console.log(err);
+    if (err) {
+    	logger.log('error', err);
       return next(error_msg.global.error);
     }
     else {
-      console.log('here');
-      /*** Query for selecting parking spot information ***/
+      // Query parking spot information
       connection.query(select_query, [req.body.location_id, req.body.parking_id], function(err, results) {
-        if(err) {
-          console.log(err);
+        if (err) {
+          logger.log('error', err);
           connection.release();
           return next(error_msg.global.error);
         }
-        if(results.length == 0) {
-          console.log(error_msg.stripe.no_parking_space);
-          connection.release();          
+        if (results.length == 0) {
+          connection.release();
           return next(error_msg.stripe.no_parking_space);
         }
         else {
-          console.log("HERE");
           //Parking spot details
           var parking_spot = results[0];
-          console.log(parking_spot);
+
+          logger.log('info', parking_spot);
+
           //Check status of parking spot
-          if(parking_spot.status == "occupied") {
+          if (parking_spot.status == "occupied") {
             //If occupied, return an error
             connection.release();
             return next(error_msg.stripe.parking_occupied);
@@ -192,8 +203,8 @@ router.post('/charge', function(req, res, next) {
 
           //Amount to be charged (1 dollar per hour)
           var amount = req.body.hours * 100;
-          //console.log(req.decoded);
-          /*** Create a charge with Stripe API ***/
+
+          // Create charge with Stripe API
           stripe.charges.create({
             amount: amount,
             currency: "USD",
@@ -201,40 +212,41 @@ router.post('/charge', function(req, res, next) {
             receipt_email: req.decoded.email,
             statement_descriptor: "Parking"
           }).then(function(charge) {
-            var update_query = "UPDATE parking_space SET status = ?, occupied_by = ?, customer_id = ?,start_time = ?, end_time = ?, transaction_id = ? WHERE location_id = ? AND space_id = ?";
-            var date = new Date();
-            var start_time = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-            date.setHours(date.getHours() + parseInt(req.body.hours));
-            var end_time = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " + date.getHours()+ ":" + date.getMinutes() + ":" + date.getSeconds();
+		      	logger.log('info', req.decoded.email + ": Successfully created charge");
+          	logger.log('info', charge);
+            var update_query = "UPDATE parking_space SET status = ?, occupied_by = ?, customer_id = ?, start_time = ?, end_time = ?, transaction_id = ? WHERE location_id = ? AND space_id = ?";
+            var start_time = new Date().toISOString().slice(0, 19).replace('T', ' ');
+            var end_time = new Date().addHours(req.body.hours).toISOString().slice(0, 19).replace('T', ' ');
             /*** Query for updating parking spot information ***/
-            connection.query(update_query, ["occupied", req.decoded.email, req.decoded.customer_id , start_time, end_time, charge.id, req.body.location_id, req.body.parking_id], function(err, results) {
-              if(err) {
-                console.log(err);
+            connection.query(update_query, ["occupied", req.decoded.email, req.decoded.customer_id, start_time, end_time, charge.id, req.body.location_id, req.body.parking_id], function(err, results) {
+              if (err) {
+                logger.log('error', err);
                 connection.release();
                 return next(error_msg.global.error);
               }
               else {
+              	logger.log('info', req.decoded.email + ": Successfully updated parking spot");
+
                 //Return success message
                 res.send(success_msg.stripe.charge_create);
-                var insert_query= "INSERT INTO transactions (transaction_id, created, customer_id, amount, failure_code, failure_message, email, invoice, paid, refunded, location_id, parking_space_id) VALUES (?, ?, ?, ?, NULL, NULL, ?, ?, ?, ?, ?, ?)";
+                var insert_query = "INSERT INTO transactions (transaction_id, created, customer_id, amount, failure_code, failure_message, email, invoice, paid, refunded, location_id, parking_space_id) VALUES (?, ?, ?, ?, NULL, NULL, ?, ?, ?, ?, ?, ?)";
                 var created = new Date();
-                //console.log(charge);
-                connection.query(insert_query, [charge.id, charge.created, charge.customer, charge.amount, req.decoded.email, charge.invoice, charge.paid, charge.refunded, req.body.location_id, req.body.parking_id], function(err, results){
-                  if(err){
+                connection.query(insert_query, [charge.id, charge.created, charge.customer, charge.amount, req.decoded.email, charge.invoice, charge.paid, charge.refunded, req.body.location_id, req.body.parking_id], function(err, results) {
+                  if (err) {
+                  	logger.log('error', err);
                     connection.release();
-                    console.log(err);
                     return next(error_msg.global.error);
                   }
-                  else{
+                  else {
+                  	logger.log('info', req.decoded.email + ": Successfully logged transaction")
                     connection.release();
-                    console.log("SUCCESS");
                   }
                 })
 
               }
             })
           }).catch(function(err) {
-            console.log(err);
+            logger.log('error', err);
             return next(error_msg.global.error);
           })
         }
@@ -243,7 +255,8 @@ router.post('/charge', function(req, res, next) {
   })
 });
 
-router.use(function (err, req, res, next) {
+router.use(function(err, req, res, next) {
+  logger.log('error', err);	
   res.status(err.status || 500);
   res.send({
     message: err.message || "Internal server error."
@@ -251,9 +264,9 @@ router.use(function (err, req, res, next) {
 })
 
 //Helper function set add hours to a date object
-Date.prototype.addHours= function(h){
-    this.setHours(this.getHours()+h);
-    return this;
+Date.prototype.addHours = function(h) {
+  this.setHours(this.getHours() + h);
+  return this;
 }
 
 function parkingPoolCreate() {
@@ -266,4 +279,3 @@ function parkingPoolCreate() {
 
 
 module.exports = router;
-
